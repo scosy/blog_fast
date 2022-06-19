8df029cb4a655965e9509ba01ea7bc0c24e7255c
scosy <amine.bentaleb064@gmail.com>
2022-06-19 15:46:05 +0200

    :white_check_mark: Added model tests for Post

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @post = Post.new(title: 'Title', body: 'This is the body of my post')
+  end
+
+  test 'post should be valid' do
+    assert @post.valid?
+  end
+
+  test "post's title should not be empty" do
+    @post.title = nil
+    assert_not @post.valid?
+  end
+
+  test "post's title should not less than 5 characters" do
+    @post.title = 'a' * 4
+    assert_not @post.valid?
+  end
 end
